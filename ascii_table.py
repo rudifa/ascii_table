baf254046b91aacaf0ba79d537d082254e82b19d
Rudi Farkas <rudi.farkas@gmail.com>
2020-03-07 14:39:45 +0100

    remove older version of converter code, use AsciiTable @staticmethod instead

--- ascii_table.py
+++ ascii_table.py
@@ -1,6 +1,6 @@
 #!/usr/local/bin/python
 #
-#  ascii_table.py
+#  ascii_table.py v.0.1.2
 #  Converts a .tsv or .csv file to an ascii table
 #
 #  Created by Rudolf Farkas on 28.02.2020
@@ -9,11 +9,8 @@
 
 import sys
 import os
-from functools import reduce
 from itertools import zip_longest
 from argparse import ArgumentParser
-
-#from ascii_table import splitme, len_of, max_fieldwidths_from, fit, linebreak_from, fields_from, string_from, limited_lines, table_limited
 
 class AsciiTable(object):
     @staticmethod
@@ -72,9 +69,8 @@ class AsciiTable(object):
         so that the resulting `table_width` is not above `max_table_width`.
         Return the reduced `maxwidths`.
         """
-        #print("maxwidths=", maxwidths, "max_table_width=", max_table_width)
         while(True):
-            table_width = table_width_for(maxwidths)
+            table_width = AsciiTable.table_width_for(maxwidths)
             if sum(maxwidths)== 0:
                 return maxwidths
             elif table_width <= max_table_width:
@@ -115,140 +111,27 @@ class AsciiTable(object):
         Returns the list of row_lines.
         """
         row_lines = []
-        while(len_of(fields) > 0):
-            field_prefixes = [splitme(field, limit)[0] for field, limit in zip(fields, fieldwidths)]
-            row_lines.append(string_from(fieldwidths, field_prefixes))
-            fields = [splitme(field, limit)[1].lstrip() for field, limit in zip(fields, fieldwidths)]
+        while(AsciiTable.len_of(fields) > 0):
+            field_prefixes = [AsciiTable.splitme(field, limit)[0] for field, limit in zip(fields, fieldwidths)]
+            row_lines.append(AsciiTable.string_from(fieldwidths, field_prefixes))
+            fields = [AsciiTable.splitme(field, limit)[1].lstrip() for field, limit in zip(fields, fieldwidths)]
         return row_lines
 
     @staticmethod
     def table_limited(lines, delim, table_width):
         """Given `lines`, `delim` and `table_width` composes and returns a multiline table string.
         """
-        max_fieldwidths = max_fieldwidths_from(lines, delim)
-        new_maxwidths = fit(max_fieldwidths, table_width)
-        linebreak = linebreak_from(new_maxwidths)
+        max_fieldwidths = AsciiTable.max_fieldwidths_from(lines, delim)
+        new_maxwidths = AsciiTable.fit(max_fieldwidths, table_width)
+        linebreak = AsciiTable.linebreak_from(new_maxwidths)
         textlines = [linebreak]
         for line in lines:
-             fields = fields_from(line, delim)
-             limited = limited_lines(new_maxwidths, fields)
+             fields = AsciiTable.fields_from(line, delim)
+             limited = AsciiTable.limited_lines(new_maxwidths, fields)
              textlines += limited
              textlines.append(linebreak)
         return "\n".join(textlines)
-
     pass
-
-# processing helpers
-
-def len_of(fields):
-    """Given fields: a list of strings, returns the length of concatenated fields.
-    """
-    return len(''.join(fields))
-
-def splitme(text, limit):
-    """Splits text into prefix and suffix
-    such that len(prefix) <= limit
-    and prefix ends with a whole word if possible,
-    while suffix contains the remainder of text, if any.
-    Returns a tuple (prefix, suffix).
-    """
-    limit = max(limit, 1)
-    if len(text) <= limit:
-        return text, ''
-    prefix = text[:limit]
-    lsx = prefix.rfind(' ')
-    if lsx >= 0:
-        return text[:lsx], text[lsx:]
-    else:
-        return text[:limit], text[limit:]
-
-def fields_from(textline, delim):
-    """ Strips textline of outer spaces, splits it on the delim character
-    into a list of fields, each stripped of its outer spaces.
-    Returns the list of fields.
-    """
-    return [field.strip() for field in textline.strip(' ').split(delim)]
-
-def max_fieldwidths_from(lines, delim):
-    """Given a list of lines and a delim that splits each line into fields
-    finds max width for each column.
-    Returns the list of column widths.
-    """
-    maxwidths = []
-    for line in lines:
-        widths = [len(field) for field in fields_from(line, delim)]
-        maxwidths = [max(m, w) for m, w in zip_longest(maxwidths, widths, fillvalue = 0)]
-    return maxwidths
-
-def fit(maxwidths, max_table_width):
-    """Given `maxwidths` and a `max_table_width`, reduce `maxwidths` values (largest first)
-    so that the resulting `table_width` is not above `max_table_width`.
-    Return the reduced `maxwidths`.
-    """
-    #print("maxwidths=", maxwidths, "max_table_width=", max_table_width)
-    while(True):
-        table_width = table_width_for(maxwidths)
-        if sum(maxwidths)== 0:
-            return maxwidths
-        elif table_width <= max_table_width:
-            return maxwidths
-        else:
-            limit = int(max(maxwidths) - (table_width - max_table_width) / len(maxwidths))
-            maxwidths = [limit if width > limit else width for width in maxwidths]
-
-def string_from(fieldwidths, linefields):
-    """Given `fieldwidths` and `linefields`
-    compose a string each field is truncated or padded if needed to the corresponding fieldwidth
-    then fields are joined by column separators and surrounded by column ends.
-    Returns the composed string.
-    """
-    out = []
-    for idx, field in enumerate(linefields):
-        fieldwidth = fieldwidths[idx]
-        out.append(field[:fieldwidth] + ' ' * (fieldwidth - len(field)))
-    return '| ' + ' | '.join(out) + ' |'
-
-def linebreak_from(fieldwidths):
-    """Given `fieldwidths` compose a `linebreak` string
-    compatible with `fieldwidths`.
-    Returns the `linebreak` string.
-    """
-    out = []
-    for width in fieldwidths:
-        out.append('-' * (width + 2))
-    return '+' + '+'.join(out) + '+'
-
-def table_width_for(maxwidths):
-    """Given `maxwidths` returns the corresponding table width.
-    """
-    return sum(maxwidths) + 3 * (len(maxwidths) - 1) + 2*2
-
-def limited_lines(fieldwidths, fields):
-    """Given `fieldwidths` and `fields` of a line
-    fold `fields` into several `row_lines` if necessary
-    such that each field remains within the corresponding `fieldwidths` value.
-    Returns the list of row_lines.
-    """
-    row_lines = []
-    while(len_of(fields) > 0):
-        field_prefixes = [splitme(field, limit)[0] for field, limit in zip(fields, fieldwidths)]
-        row_lines.append(string_from(fieldwidths, field_prefixes))
-        fields = [splitme(field, limit)[1].lstrip() for field, limit in zip(fields, fieldwidths)]
-    return row_lines
-
-def table_limited(lines, delim, table_width):
-    """Given `lines`, `delim` and `table_width` composes and returns a multiline table string.
-    """
-    max_fieldwidths = max_fieldwidths_from(lines, delim)
-    new_maxwidths = fit(max_fieldwidths, table_width)
-    linebreak = linebreak_from(new_maxwidths)
-    textlines = [linebreak]
-    for line in lines:
-         fields = fields_from(line, delim)
-         limited = limited_lines(new_maxwidths, fields)
-         textlines += limited
-         textlines.append(linebreak)
-    return "\n".join(textlines)
 
 def lines_from(filehandle):
     """Given a `filehandle` to an open text file
@@ -267,16 +150,10 @@ def string_table_limited(filehandle, delim, table_width):
     lines = lines_from(filehandle)
     return AsciiTable.table_limited(lines, delim, table_width)
 
-# next:
-# init AsciiTable instance with lines, delim
-# get table(width); implement it using properties lines and delim
-
 def run(args):
     with open(args.filepath,'r') as fh:
         table_limited2 = string_table_limited(fh, delim, args.w)
         print(table_limited2)
-
-# python ascii_table.py tmp/table.tsv
 
 if __name__ == "__main__":
     parser = ArgumentParser(description='Converts a .tsv or .csv file to an ascii table.\nPrints the result to stdout.')
@@ -295,4 +172,4 @@ if __name__ == "__main__":
             delim = ","
         elif ext == ".tsv":
             delim = "\t"
-    run(args)
+        run(args)
--- ascii_table_unittest.py
+++ ascii_table_unittest.py
@@ -1,6 +1,6 @@
 #!/usr/local/bin/python
 #
-#  ascii_table_unittest.py
+#  ascii_table_unittest.py v.0.1.2
 #  Tests for ascii_table.py
 #
 #  Created by Rudolf Farkas on 28.02.2020
@@ -8,7 +8,6 @@
 #
 
 import unittest
-from ascii_table import splitme, len_of, max_fieldwidths_from, table_width_for, fit, linebreak_from, fields_from, string_from, limited_lines, table_limited
 from ascii_table import AsciiTable
 
 class ascii_table_unittest(unittest.TestCase):
@@ -18,123 +17,6 @@ class ascii_table_unittest(unittest.TestCase):
         self.tsv_3_lines = ["state	s.e	event	action	next state",
                 ".idle  0 cells selected	0.0	hit a free cell	select cell	.booking1",
                 "	0.1	hit a cell in a current user's booking	select cell	.cancelling"]
-
-    def test_splitme(self):
-
-        input = "deselect the selected"
-        self.assertEqual(len(input), 21)
-        limit = 25
-        self.assertEqual(splitme(input, limit),  ('deselect the selected', ''))
-        limit = 21
-        self.assertEqual(splitme(input, limit),  ('deselect the selected', ''))
-        limit = 20
-        self.assertEqual(splitme(input, limit),  ('deselect the', ' selected'))
-        limit = 15
-        self.assertEqual(splitme(input, limit),  ('deselect the', ' selected'))
-        limit = 12
-        self.assertEqual(splitme(input, limit),  ('deselect', ' the selected'))
-        limit = 11
-        self.assertEqual(splitme(input, limit),  ('deselect', ' the selected'))
-        limit = 9
-        self.assertEqual(splitme(input, limit),  ('deselect', ' the selected'))
-        limit = 8
-        self.assertEqual(splitme(input, limit),  ('deselect', ' the selected'))
-        limit = 7
-        self.assertEqual(splitme(input, limit),  ('deselec', 't the selected'))
-        limit = 1
-        self.assertEqual(splitme(input, limit),  ('d', 'eselect the selected'))
-        limit = 0
-        self.assertEqual(splitme(input, limit),  ('d', 'eselect the selected'))
-        limit = -7
-        self.assertEqual(splitme(input, limit),  ('d', 'eselect the selected'))
-
-        input = ""
-        self.assertEqual(len(input), 0)
-        limit = 5
-        self.assertEqual(splitme(input, limit),  ('', ''))
-        limit = 0
-        self.assertEqual(splitme(input, limit),  ('', ''))
-        limit = -5
-        self.assertEqual(splitme(input, limit),  ('', ''))
-
-    def test_len_of(self):
-        input = ['']
-        self.assertEqual(len_of(input), 0)
-        input = ['', '0.1', 'select cell', '.cancelling']
-        self.assertEqual(len_of(input), 25)
-
-    def test_fields_from(self):
-        delim = '\t'
-        input = "	0.1	hit a cell in a current user's booking	select cell	.cancelling"
-        self.assertEqual(fields_from(input, delim), ['', '0.1', "hit a cell in a current user's booking", 'select cell', '.cancelling'])
-        input = ""
-        self.assertEqual(fields_from(input, delim), [''])
-        input = "\t"
-        self.assertEqual(fields_from(input, delim), ['', ''])
-
-    def test_max_fieldwidths_from(self):
-        delim = '\t'
-        table_width = 80
-        input = self.tsv_3_lines
-        self.assertEqual(max_fieldwidths_from(self.tsv_3_lines, self.tsv_delim), [23, 3, 38, 11, 11])
-
-    def test_table_width_for(self):
-        fieldwidths = [23, 3, 38, 11, 11]
-        output = table_width_for(fieldwidths)
-        self.assertLessEqual(output, 120)
-        self.assertEqual(output, 102)
-        fieldwidths = [10, 3, 10, 10, 10]
-        output = table_width_for(fieldwidths)
-        self.assertLessEqual(output, 60)
-        self.assertEqual(output, 59)
-
-    def test_fit(self):
-        max_fieldwidths = [23, 3, 38, 11, 11]
-        table_width = 120
-        self.assertEqual(fit(max_fieldwidths, table_width), [23, 3, 38, 11, 11])
-        table_width = 60
-        self.assertEqual(fit(max_fieldwidths, table_width), [10, 3, 10, 10, 10])
-
-    def test_linebreak_from(self):
-        maxwidths = [10, 3, 10, 10, 10]
-        self.assertEqual(linebreak_from(maxwidths), '+------------+-----+------------+------------+------------+')
-
-    def test_string_from(self):
-        maxwidths = [10, 3, 10, 10, 10]
-        fields = ['', '0.1', "hit a cell in a current user's booking", 'select cell', '.cancelling']
-        output = string_from(maxwidths, fields)
-        self.assertEqual(output, "|            | 0.1 | hit a cell | select cel | .cancellin |")
-
-    def test_limited_lines(self):
-        maxwidths = [10, 3, 10, 10, 10]
-        fields = fields_from(self.tsv_3_lines[2], self.tsv_delim)
-        expected = ['|            | 0.1 | hit a      | select     | .cancellin |',
-                    '|            |     | cell in a  | cell       | g          |',
-                    '|            |     | current    |            |            |',
-                    "|            |     | user's     |            |            |",
-                    '|            |     | booking    |            |            |']
-        output = limited_lines(maxwidths, fields)
-        self.assertEqual(output, expected)
-
-    def test_table_limited(self):
-        table_width = 50
-        input = ['']
-        self.assertEqual(table_limited(input, self.tsv_delim, table_width), '+--+\n+--+')
-        table_width = 80
-        #self.maxDiff = None #<- uncomment to see long string differences
-        expected = """\
-+---------------------+-----+---------------------+-------------+-------------+
-| state               | s.e | event               | action      | next state  |
-+---------------------+-----+---------------------+-------------+-------------+
-| .idle  0 cells      | 0.0 | hit a free cell     | select cell | .booking1   |
-| selected            |     |                     |             |             |
-+---------------------+-----+---------------------+-------------+-------------+
-|                     | 0.1 | hit a cell in a     | select cell | .cancelling |
-|                     |     | current user's      |             |             |
-|                     |     | booking             |             |             |
-+---------------------+-----+---------------------+-------------+-------------+"""
-        self.assertEqual(table_limited(self.tsv_3_lines, self.tsv_delim, table_width), expected)
-        #print("\n"+table_limited(input, delim, table_width))
 
 class AsciiTable_unittest(unittest.TestCase):
 
@@ -231,7 +113,7 @@ class AsciiTable_unittest(unittest.TestCase):
 
     def test_limited_lines(self):
         maxwidths = [10, 3, 10, 10, 10]
-        fields = fields_from(self.tsv_3_lines[2], self.tsv_delim)
+        fields = AsciiTable.fields_from(self.tsv_3_lines[2], self.tsv_delim)
         expected = ['|            | 0.1 | hit a      | select     | .cancellin |',
                     '|            |     | cell in a  | cell       | g          |',
                     '|            |     | current    |            |            |',
@@ -257,7 +139,7 @@ class AsciiTable_unittest(unittest.TestCase):
 |                     |     | current user's      |             |             |
 |                     |     | booking             |             |             |
 +---------------------+-----+---------------------+-------------+-------------+"""
-        self.assertEqual(table_limited(self.tsv_3_lines, self.tsv_delim, table_width), expected)
+        self.assertEqual(AsciiTable.table_limited(self.tsv_3_lines, self.tsv_delim, table_width), expected)
         #print("\n"+table_limited(input, delim, table_width))
 
 if __name__ == "__main__":
